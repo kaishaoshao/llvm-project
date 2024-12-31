c0e107768e58d389131200c7bfc58ad252018c6e
kaishaoshao <2032751077@qq.com>
2024-12-31 16:15:09 +0800

    suffcesful add mypass

--- llvm/lib/Transforms/MyPass/MyPass.cpp
+++ llvm/lib/Transforms/MyPass/MyPass.cpp
@@ -1,16 +1,11 @@
-// MyPass.cpp
-
 #include "llvm/Transforms/MyPass/MyPass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
-PreservedAnalyses MyPass::run(Function &F,
-                    FunctionAnalysisManager &AM)
-{
-    errs() << "MyPass in function: " << F.getName() << "\n";
-
-    return PreservedAnalyses::all();
+PreservedAnalyses MyPass::run(Function &F, FunctionAnalysisManager &AM) {
+  errs() << "MyPass in function: " << F.getName().str() << "\n";
+  return PreservedAnalyses::all();
 }
-
-
