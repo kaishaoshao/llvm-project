1f9b4393d6d88fac6df356821e22324fb2300ef4
shaokai.wang <shaokai.wang@terapines.com>
2025-02-17 18:20:14 +0800

    3.1 Cpu0TargetMachine.h/.cpp

--- cpu0_llvm12/llvm/modify/llvm/lib/Target/Cpu0/Cpu0TargetMachine.cpp
+++ cpu0_llvm12/llvm/modify/llvm/lib/Target/Cpu0/Cpu0TargetMachine.cpp
@@ -14,6 +14,16 @@
 #include "Cpu0TargetMachine.h"
 #include "Cpu0.h"
 
+// #include "llvm/IR/Attributes.h"
+// #include "llvm/IR/Function.h"
+// #include "llvm/Support/CodeGen.h"
+// #include "llvm/CodeGen/Passes.h"
+// #include "llvm/CodeGen/TargetPassConfig.h"
+// #include "llvm/Support/TargetRegistry.h"
+// #include "llvm/Target/TargetOptions.h"
+
+#include "Cpu0Subtarget.h"
+#include "Cpu0TargetObjectFile.h"
 #include "llvm/IR/Attributes.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/CodeGen.h"
@@ -26,6 +36,82 @@ using namespace llvm;
 
 #define DEBUG_TYPE "cpu0"
 
+
+
 extern "C" void LLVMInitializeCpu0Target() {
+    // Register the target.
+    //- Big endian Target Machine
+    RegisterTargetMachine<Cpu0TargetMachine> X(TheCpu0Target);
+    // - Little endian Target Machine
+    RegisterTargetMachine<Cpu0TargetMachine> Y(TheCpu0elTarget);
 }
+
+static std::string computeDataLayoutString(const Triple &TT, StringRef CPU,
+                                           const TargetOptions &Options,
+                                           bool isLittle) {
+    std::string Ret = "";
+    // There are both little and big endian Cpu0.
+    if (isLittle)
+        Ret += "e";
+    else
+        Ret += "E";
+    
+    Ret += "-m:m";  // 机器模式
+
+    // Pointers are 32 bits some ABIs
+    Ret += "-p:32:32";
+
+    // 8 and 16 bit integers only need to have natural alignment, but try to 
+    // align them to 32 bits. 64 bit integers have natural alignment.
+
+    Ret += "-i8:8:32-i16:16:32-i64:64"; // 整数对齐规则
+
+    // 32 bit registers are always available and the stack is at least 64 aligned.
+    Ret += "-n32-S64";  // 添加寄存器和栈对齐规则
+
+    return Ret;
+}
+
+// 获取有效的重定位模型
+static Reloc::Model getEffectiveCodeModel(bool JIT,
+                                        Optional<Reloc::Model> RM){
+    if (!RM.hasValue())
+        return Reloc::Static;
+    return *RM;
+}
+
+// DataLayout --> Big-endian, 32-bit pointer/ABI/alignment
+// The stack is always 8 byte aligned
+// On function prologue, the stack is created by decrementing
+// its pointer. Once decremented, all references are done with positive
+// offset from the stack/frame pointer, using StackGrowsUp enables
+// an easier handling.
+// Using CodeModel::Large enables different CALL behavior.
+CpuoTargetMachine::Cpu0TargetMachine(const Target &T, const Triple &TT,
+                                     StringRef CPU, StringRef FS,
+                                     const TargetOptions &Options,
+                                     Optional<Reloc::Model> RM, 
+                                     Optional<CodeModel::Model> CM,
+                                     CodeGenOpt::Level OL, bool JIT, 
+                                     bool isLittle) 
+// 默认大端
+    : LLVMTargetMachine(T, computeDataLayout(TT, CPU, Options, isLittle), TT,
+                        CPU, FS, Options, getEffectiveCodeModel(JIT, RM),
+                        getEffectiveCodeModel(CM, CodeModel::Small), OL),
+        isLittle(isLittle), TLOF(make_unique<Cpu0TargetObjectFile>()),
+        ABI(Cpu0ABI::computeTargetABI()), 
+        DefaultSubtarget(TT, CPU, FS, isLittle, *this){
+    // initAsmInfo will display featurea by llc -march=cpu0 -mcpu=help on 3.7 but not on 3.6
+    initsAsmInfo();
+}
+
+Cpu0TargetMachine::~Cpu0TargetMachine() {}
+
+viod Cpu0TargetMachine::anchor() {}
+
+
+
+              
+
+
 
--- cpu0_llvm12/llvm/modify/llvm/lib/Target/Cpu0/Cpu0TargetMachine.h
+++ cpu0_llvm12/llvm/modify/llvm/lib/Target/Cpu0/Cpu0TargetMachine.h
@@ -11,21 +11,93 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "Cpu0TargetMachine.h"
-#include "Cpu0.h"
+#ifndef LLVM_LIB_TARGET_CPU0_CPU0TARGETMACHINE_H
+#define LLVM_LIB_TARGET_CPU0_CPU0TARGETMACHINE_H
+// #include "Cpu0TargetMachine.h"
+// #include "Cpu0.h"
 
-#include "llvm/IR/Attributes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/Support/CodeGen.h"
-#include "llvm/CodeGen/Passes.h"
-#include "llvm/CodeGen/TargetPassConfig.h"
-#include "llvm/Support/TargetRegistry.h"
-#include "llvm/Target/TargetOptions.h"
+// #include "llvm/IR/Attributes.h"
+// #include "llvm/IR/Function.h"
+// #include "llvm/Support/CodeGen.h"
+// #include "llvm/CodeGen/Passes.h"
+// #include "llvm/CodeGen/TargetPassConfig.h"
+// #include "llvm/Support/TargetRegistry.h"
+// #include "llvm/Target/TargetOptions.h"
 
-using namespace llvm;
 
-#define DEBUG_TYPE "cpu0"
+#include "Cpu0Config.h" // 包含Cpu0配置文件
 
-extern "C" void LLVMInitializeCpu0Target() {
-}
+#include "MCTargetDesc/Cpu0ABIInfo.h" // 包含Cpu0 ABI信息
+#include "Cpu0Subtarget.h" // 包含Cpu0子目标信息
+#include "llvm/CodeGen/Passes.h" // 包含代码生成相关的头文件
+#include "llvm/CodeGen/SelectionDAGISel.h" // 包含选择DAG指令选择器的头文件
+#include "llvm/CodeGen/TargetFrameLowering.h" // 包含目标框架降低的头文件
+#include "llvm/Support/CodeGen.h" // 包含代码生成支持的头文件
+#include "llvm/Target/TargetMachine.h" // 包含目标机器的头文件
 
+namespace llvm {
+class formatted_raw_ostream; // 前向声明formatted_raw_ostream类
+class Cpu0RegisterInfo; // 前向声明Cpu0RegisterInfo类
+
+// 定义Cpu0TargetMachine类，继承自LLVMTargetMachine
+class Cpu0TargetMchine : public LLVMTargetMachine {
+    bool isLittle; // 是否为小端模式
+
+    std::unique_ptr<TargetLoweringObjectFile> TLOF; // 独占指针，指向目标降低对象文件
+
+    // 选择的ABI
+    Cpu0ABI::ABI TargetABI;
+    Cpu0Subtarget DefaultSubtarget; // 默认子目标
+
+    mutable StringMap<std::unique_ptr<Cpu0Subtarget>> SubtargetMap; // 可变的子目标映射
+
+public:
+    // 构造函数
+    Cpu0TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                      StringRef FS, const TargetOptions &Options,
+                      Optional<Reloc::Model> RM, CodeModel::Model CM,
+                      CodeGenOpt::Level OL, bool JIT, bool isLittle);
+    // 析构函数
+    ~Cpu0TargetMachine() override;
+
+    // 获取子目标实现
+    const Cpu0Subtarget *getSubtargetImpl() const { 
+        return &DefaultSubtarget; 
+    }
+
+    // 根据函数获取子目标实现
+    const Cpu0Subtarget *getSubtargetImpl(const Function &F) const override;
+
+    // 传递管道配置
+    TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
+
+    // 获取对象文件降低
+    TargetLoweringObjectFile *getObjFileLowering() const override {
+        return TLOF.get();
+    }
+
+    // 判断是否为小端模式
+    bool isLittleEndian() const { return isLittle; }
+
+    // 获取ABI信息
+    const Cpu0ABIInfo *getABI() const { return ABI; }
+
+};
+
+// Cpu0TagetMachine - Cpu0小端目标机器
+class Cpu0elTargetMachine : public Cpu0TargetMachine {
+    virtual void anchor(); // 锚定函数
+public:
+    // 构造函数
+    Cpu0elTargetMachine(const Target &T, const Triple &TT, StringRef CPU,
+                        StringRef FS, const TargetOptions &Options,
+                        Optional<Reloc::Model> RM, CodeModel::Model CM,
+                        CodeGenOpt::Level OL, bool JIT);
+
+};
+
+}  // 结束llvm命名空间
+
+#endif // 结束预处理指令
+
+#endif
