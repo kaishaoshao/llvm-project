389454db19311aab49a2e8b8f6b31e1776ea7cbd
shaokai.wang <shaokai.wang@terapines.com>
2025-03-18 14:52:46 +0800

    [RISCVCPU][ch2] [llvm/lib/Target/RISCVCPU/] RISCVCPUTargetMachine.cpp RISCVCPUTargetMachine.h 创建了RISCVCPUTargetMachine类继承于LLVMTargetMachine,并定义初始化函数,
    [finish]
    实现LLVMInitializeRISCVCPUTarget.RegisterTargetMachine会设置TheTaget的
    TargetMachineCtorFn,使得TheTarget->createTargetMachine返回ToyTargetMachine实例
    
    [ERROR]
    llc: xx/llvm-project/llvm/lib/CodeGen/LLVMTargetMachine.cpp:42:
    void llvm::LLVMTargetMachine::initAsmInfo(): Assertion
    `MRI && "Unable to create reg info"' failed.
    
    [TODO]
    出错原因是没有调用RegisterMCRegInfo,导致initAsmInfo时出错

--- llvm/lib/Target/RISCVCPU/RISCVCPUTargetMachine.cpp
+++ llvm/lib/Target/RISCVCPU/RISCVCPUTargetMachine.cpp
@@ -1,3 +1,23 @@
+#include "RISCVCPUTargetMachine.h"
+
+#include <llvm/MC/TargetRegistry.h>
+
+using namespace llvm;
+
+extern Target TheRISCVCPUTarget;
+
 
 ///  用于初始化RISCV CPU目标
-extern "C" void LLVMInitializeRISCVCPUTarget() {}
+extern "C" void LLVMInitializeRISCVCPUTarget() {
+    RegisterTargetMachine<RISCVCPUTargetMachine> X(TheRISCVCPUTarget);
+}
+
+RISCVCPUTargetMachine::RISCVCPUTargetMachine(Target const &T, 
+        Triple const &TT, StringRef CPU, StringRef FS,
+        TargetOptions const &Options, std::optional<Reloc::Model> RM,
+        std::optional<CodeModel::Model> CM, CodeGenOpt::Level OL,
+        bool JIT) :  // 这里的参数主要是 llc 传递过来的命令行参数 
+        LLVMTargetMachine(T, "e-m:m-p:32:32-i8:8:32-i16:16:32-i64:64-n32-S64", 
+            TT, CPU, FS, Options, Reloc::Static, CodeModel::Small, OL) {
+        initAsmInfo();
+}
---
+++ llvm/lib/Target/RISCVCPU/RISCVCPUTargetMachine.h
@@ -0,0 +1,47 @@
+#ifndef __RISCVCPU_TARGET_MACHINE_H__
+#define __RISCVCPU_TARGET_MACHINE_H__
+
+#include "llvm/Target/TargetMachine.h"
+
+///
+///
+/// | 参数	                         |    影响范围	       |  典型值示例
+/// | Target const &T	            | 目标架构和名称       | TheRISCVTarget
+/// | Triple const &TT              | 目标平台特性	       | riscv64-unknown-linux-gnu
+/// | StringRef CPU	                | CPU型号和指令集扩展   | 	generic-rv64
+/// | StringRef FS	                | 目标特性字符串	   | +m,+a,+f
+/// | TargetOptions const &Options  | 高级代码生成选项	   | TargetOptions()
+/// | Reloc::Model RM               | 重定位模型	      | Reloc::Static
+/// | CodeModel::Model CM           | 代码模型	          | CodeModel::Small
+/// | CodeGenOpt::Level OL          | 优化级别	          | CodeGenOpt::Default
+/// | bool JIT	                    | JIT编译标志         | false
+///
+
+namespace llvm {
+class RISCVCPUTargetMachine : public LLVMTargetMachine {
+public:
+    /// @brief RISCVCPUTargetMachine 是LLVM中用于表示RISCVCPU目标机器的类，
+    ///                 其构造函数参数用于配置目标机器的各种属性 
+    /// @param T        表示目标机器的基本描述信息 如 riscvcpu
+    /// @param TT       表示目标三元组信息 如 riscv64-unknown-linux-gnu
+    /// @param CPU      指定目标CPU的具体型号或特性 如 sifive-u74 
+    ///                 用于优化代码生成（如启用特定CPU的指令调度策略
+    /// @param FS       指定目标平台的特性字符串（Feature String），控制目标平台的指令集扩展
+    ///                 启用或禁用某些目标特性（如+c表示压缩指令，-m表示禁用乘法指令）
+    /// @param Options  配置目标机器的高级选项： 是否使用软件浮点模拟 PIE POSIC  控制代码生成的高级行为
+    /// @param RM       指定重定位模型（Relocation Model）控制生成的代码是否支持动态链接
+    /// @param CM       指定代码模型（Code Model） CodeModel::Small：小代码模型（默认）
+    /// @param OL       指定代码生成优化级别  CodeGenOpt::Default：默认优化
+    /// @param JIT      指定目标机器是否用于JIT（Just-In-Time）编译
+    RISCVCPUTargetMachine(Target const &T, Triple const &TT, 
+            StringRef CPU, StringRef FS, TargetOptions const &Options,
+            std::optional<Reloc::Model> RM, std::optional<CodeModel::Model> CM,
+            CodeGenOpt::Level OL, bool JIT);
+    ~RISCVCPUTargetMachine() {};
+
+};
+
+}
+
+
+#endif // __RISCVCPU_TARGET_MACHINE_H__
