9c742a36c7474a6f25275506c57a979110e69988
kaishaoshao <2032751077@qq.com>
2025-05-18 18:23:45 +0800

    [build]: add mips_build.sh

---
+++ mips_build.sh
@@ -0,0 +1,8 @@
+mkdir -p build
+cmake -S llvm -B build -G Ninja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLLVM_BUILD_TESTS=ON \
+      -DLLVM_PARALLEL_LINK_JOBS=8 \
+      -DLLVM_TARGETS_TO_BUILD='ARM;Mips' # Nova won't work yet
+
+cmake --build build --target llc 
