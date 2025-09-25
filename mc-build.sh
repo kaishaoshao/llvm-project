a65bb1e434a70ce2732bc329af434ff45abaca5e
kaishaoshao <2032751077@qq.com>
2025-09-25 10:30:17 +0800

    test

--- mc-build.sh
+++ mc-build.sh
@@ -2,8 +2,8 @@ mkdir -p build
 cd build
 cmake -G Ninja                                     \
       -DCMAKE_BUILD_TYPE=Release                   \
-      -DLLVM_ENABLE_PROJECTS="clang"               \
-      -DLLVM_TARGETS_TO_BUILD="X86;RISCV;MC_RISCV" \
+      -DLLVM_TARGETS_TO_BUILD="RISCV;MC_RISCV"     \
+      -DLLVM_TOOLS_TO_BUILD="llvm-mc"              \
       -DLLVM_ENABLE_TERMINFO=OFF                   \
       ../llvm
 ninja -j8  llvm-mc
